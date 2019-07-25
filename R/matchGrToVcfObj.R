7f77ed2fd515d52dd008034b69a90421753c0f24
Shilin Zhao <zhaoshilin@gmail.com>
2019-07-25 07:16:41 +0000

    match vcf working

--- R/matchGrToVcfObj.R
+++ R/matchGrToVcfObj.R
@@ -1,19 +1,17 @@
 #' @export
 #'
 matchGrToVcfObj=function(gr,vcfYield,perfectMatch=FALSE,extracVcfCols=NULL,extracVcfInfos=NULL,includingVcfPos=TRUE) {
-  if (is.null(extracVcfCols)) {
-    vcfOutTable=mcols(vcfYield)
-  } else {
-    vcfOutTable=mcols(vcfYield)[,extracVcfCols,drop=FALSE]
+  vcfOutTable=fixed(vcfYield)
+  if (!is.null(extracVcfCols)) {
+    vcfOutTable=cbind(mcols(vcfYield)[,extracVcfCols,drop=FALSE])
   }
   if (!is.null(extracVcfInfos)) {
     vcfInfosTable=info(vcfYield)[,extracVcfInfos,drop=FALSE]
-
     vcfOutTable=cbind(vcfOutTable,vcfInfosTable)
   }
   if (includingVcfPos) {
-    vcfPosTable=data.table(chr=as.character(seqnames(vcfYield)),start=start((vcfYield)),end=end((vcfYield)),Ref=as.character(fixed(vcfYield)$REF),Alt=as.character(fixed(vcfYield)$ALT))
-
+#    vcfPosTable=data.table(chr=as.character(seqnames(vcfYield)),start=start((vcfYield)),end=end((vcfYield)),Ref=as.character(fixed(vcfYield)$REF),Alt=as.character(fixed(vcfYield)$ALT))
+    vcfPosTable=data.table(chr=as.character(seqnames(vcfYield)),start=start((vcfYield)),end=end((vcfYield)))
     vcfOutTable=cbind(vcfPosTable,vcfOutTable)
   }
 
@@ -28,14 +26,17 @@ matchGrToVcfObj=function(gr,vcfYield,perfectMatch=FALSE,extracVcfCols=NULL,extra
   return(matchedListVcfCols)
 }
 
-
-readVcfAndMatchGr=function(vcfFile,variantsTableGRange,variantsTableInVcfAnnotation=NULL,genomeVersion="hg19") {
+#' @export
+#'
+readVcfAndMatchGr=function(vcfFile,variantsTableGRange,variantsTableInVcfAnnotation=NULL,genomeVersion="hg19",extracVcfInfos="AF") {
   #  vcfFile="/scratch/cqs/references/TOPMed/bravo-dbsnp-all.vcf.gz"
-  readVcfParam <- ScanVcfParam(info="AF", geno=NA,fixed=c("ALT","FILTER"))
+  extracVcfInfos=intersect(row.names(info(header(vcf))),extracVcfInfos)
+
+  readVcfParam <- ScanVcfParam(info=extracVcfInfos, geno=NA,fixed=c("ALT","FILTER"))
 
   if (is.null(variantsTableInVcfAnnotation)) {
-    variantsTableInVcfAnnotation=data.table(matrix("",ncol=7,nrow=length(variantsTableGRange)))
-    colnames(variantsTableInVcfAnnotation)=c("chr","start","end","Ref","Alt","FILTER","AF")
+    variantsTableInVcfAnnotation=data.table(matrix("",ncol=6+length(extracVcfInfos),nrow=length(variantsTableGRange)))
+    colnames(variantsTableInVcfAnnotation)=c("chr","start","end","REF","ALT","FILTER",extracVcfInfos)
   }
 
   tab <- TabixFile(vcfFile, yieldSize=50000)
@@ -51,13 +52,13 @@ readVcfAndMatchGr=function(vcfFile,variantsTableGRange,variantsTableInVcfAnnotat
     vcfYield <- VariantAnnotation::expand(x = vcfYield, row.names = TRUE)
     #rowRanges(vcfYield)
 
-    grInVcf=matchGrToVcfObj(variantsTableGRange,vcfYield,perfectMatch=FALSE,extracVcfCols="FILTER",extracVcfInfos="AF",includingVcfPos=TRUE)
+    grInVcf=matchGrToVcfObj(variantsTableGRange,vcfYield,perfectMatch=FALSE,extracVcfInfos=extracVcfInfos,includingVcfPos=TRUE)
     if (length(grInVcf)>0) {
-      grInVcfOut=sapply(grInVcf,function(x) if (any(x[,6]!="PASS")) {unlist(x[which(x[,6]!="PASS")[1],])} else {unlist(as.data.frame(x[which.max(unlist(x$AF)),]))})
+      #grInVcfOut=sapply(grInVcf,function(x) if (any(x[,6]!="PASS")) {unlist(x[which(x[,6]!="PASS")[1],])} else {unlist(as.data.frame(x[which.max(unlist(x$AF)),]))})
+      grInVcfOut=sapply(grInVcf,function(x) if (any(x[,"FILTER"]!="PASS")) {unlist(x[which(x[,"FILTER"]!="PASS")[1],])} else {unlist(as.data.frame(x[1,]))})
       grInVcfOut=data.table(t(grInVcfOut))
       row.names(grInVcfOut)=names(grInVcf)
-      #     print(head(grInVcfOut))
-      print(names(grInVcf))
+      #print(names(grInVcf))
       #variantsTableInVcfAnnotation[as.integer(row.names(grInVcfOut)),]=grInVcfOut
 
       set(variantsTableInVcfAnnotation,as.integer(row.names(grInVcfOut)),names(variantsTableInVcfAnnotation),grInVcfOut)
