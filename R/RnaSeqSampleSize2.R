5618fc51ed99d263e542c4caa39d8674a7a1e768
ShilinZhao <zhaoshilin@gmail.com>
2020-01-22 00:59:12 -0600

    RnaSeqSampleSize

--- R/RnaSeqSampleSize2.R
+++ R/RnaSeqSampleSize2.R
@@ -1,22 +1,29 @@
 
+
+
 require(ssanv)
 #Beta0: read count; Beta1: fold change
-est_one=function(Beta0=log(5),Beta1=log(2),phi=0.2,d=log(1),sampleRep=2,error=1.9*10^(-7)) {
+
+designFactorList=list()
+betaToData=function(Beta0=log(5),Beta1=log(2),phi=0.2,
+                    #d=log(1),
+                    sampleRep=2,error=1.9*10^(-7)) {
   NN=sampleRep*2
   Tret<-gl(2,sampleRep,sampleRep*2)
   design.x<-model.matrix(~Tret)
   Beta<-matrix(c(Beta0, Beta1), 2, 1)
-  mu<-as.matrix( exp(design.x %*% Beta))
+  mu<-( exp(design.x %*% Beta))
   max.mu<-max( mu )
-  J<-uniroot.integer( function(k) pnbinom(k, mu=max.mu, size=1/phi, lower.tail=F)-error,
-                      interval=c(1, 999999), maxiter=1000,step.power = 12,print.steps=TRUE)$root
+  #J<-uniroot.integer( function(k) pnbinom(k, mu=max.mu, size=1/phi, lower.tail=F)-error,
+  #                    interval=c(1, 999999), maxiter=1000,step.power = 12,print.steps=TRUE)$root
+  J=qnbinom(error, mu=max.mu, size=1/phi,lower.tail = FALSE)
 
   my_dnb<-function(x, y) dnbinom(x=x, mu=y, size=1/phi)
-  wij<-as.vector( outer(0:J, mu[1:dim(design.x)[1],], my_dnb) )
+  wij<-as.vector( outer(0:J, mu, my_dnb) )
   design.X<-design.x[rep(1:NN, each=(J+1)), ]
   row.names(design.X)<-seq(1,NN*(J+1))
-  exemplary.data<-data.frame( y=rep(0:J, times=NN), design.X[,-1], weight=wij )
-
+  exemplary.data<-data.frame( y=rep(0:J, times=NN), design.X[,-1,drop=FALSE], weight=wij )
+  return(exemplary.data)
 }
 
 get.v<-function(mybeta, error=1.9*10^(-7)){
@@ -39,12 +46,13 @@ get.v<-function(mybeta, error=1.9*10^(-7)){
   design.X<-design.x[rep(1:NN, each=(J+1)), ]
   row.names(design.X)<-seq(1,NN*(J+1))
   exemplary.data<-data.frame( y=rep(0:J, times=NN), design.X[,-1], weight=wij )
-  browser()
 
   require(MASS)
   require(ssanv)
-  fit1<-glm.nb(y~Tret2+Type2,  weights=weight, data=exemplary.data)
-  fit0<-glm.nb(y~Type2, weights=weight, data=exemplary.data)
+#  fit1<-glm.nb(y~Tret2+Type2,  weights=weight, data=exemplary.data)
+#  fit0<-glm.nb(y~Type2, weights=weight, data=exemplary.data)
+  fit1<-glm.nb(y~Tret2,  weights=weight, data=exemplary.data)
+  fit0<-glm.nb(y~1, weights=weight, data=exemplary.data)
   v<- -2*(logLik(fit0)[1]-logLik(fit1)[1])
   return(v)
 }
