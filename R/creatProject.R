abd89401f837b2deaed57939406b6b8f0d53d872
ShilinZhao <zhaoshilin@gmail.com>
2020-02-13 20:12:20 -0600

    creat project update

--- R/creatProject.R
+++ R/creatProject.R
@@ -7,8 +7,8 @@
 #' @export
 #'
 creatProject <- function(pi,project,note="",projectDate=gsub("-","",Sys.Date()),
-                         sourcePDir="d:/source/r_cqs",
-                         dataPDir="D:/OneDriveWork/OneDriveVanderbilt/work",
+                         sourcePDir=paste0(Sys.getenv("SOURCEDIR","d:/source/"),"/r_cqs"),
+                         dataPDir=Sys.getenv("WORKDIR","D:/OneDriveWork/OneDriveVanderbilt/work"),
                          templateFile="D:/source/r_cqs/myPkg/example/reportExample.Rmd") {
   projectName=paste0(pi,"/",projectDate,"_",project)
   sourceDir=paste0(sourcePDir,"/",projectName)
